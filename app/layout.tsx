dcb36121609518272ae27772b100e1a39dc12747
ambitrack-systems <ambitrack.systems@hotmail.com>
2026-01-17 12:33:33 -0500

    feat: add theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import Head from "next/head";
 
+import { ThemeProvider } from "next-themes";
+
 export const metadata: Metadata = {
   title: "AmbiTrack",
   description:
@@ -30,12 +32,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <body>{children}</body>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
